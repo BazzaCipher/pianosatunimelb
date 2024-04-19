01d3211891dc6c8d57ce17afe03643a368e1db6b
BazzaCipher <33623276+BazzaCipher@users.noreply.github.com>
2024-04-19 16:00:32 +1000

    Initial

---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<svelte:head>
+	<title>Pianos at Melbourne Uni</title>
+</svelte:head>
+
+<h1>Pianos @ Unimelb</h1>
+<ul style = "list-style-type:disk;margin: 20px">
+<li>Law Building - ground floor</li>
+<li>Market Hall, The Gallery - level 1</li>
+<li>Market Hall, Room 102 - level 1</li>
+<li>Old Architecture, Lower Theatre B151 - basement 1</li>
+<li>Union House? - level 2</li>
+</ul>
+
+<style>
+li {
+	font-size: 100%;
+}
+li + li {
+	margin-top: 20px;
+}
+</style>
